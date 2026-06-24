122ac0edbaab6f6581ff2d6d60b9164a289989f1
Vorda <davor.puzak@hotmail.com>
2026-06-24 18:54:53 +0200

    Scaffold SoftwareRRTheme from SafeMode base (razor-enabled) and reference it from main web project

---
+++ src/OrchardCore.Themes/SoftwareRRTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "SoftwareRRTheme",
+    Author = "Davor Puzak",
+    Website = "https://sofwareRR.com",
+    Version = "0.0.1",
+    Description = "Main theme for softwareRR.com"
+)]
